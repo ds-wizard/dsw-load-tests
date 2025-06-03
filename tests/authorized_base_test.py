0bef664cc5152fa306be66087dbe5f3236e46ec0
Vojtech Knaisl <vknaisl@gmail.com>
2025-06-03 16:33:46 +0200

    Fixed connection exception

--- tests/authorized_base_test.py
+++ tests/authorized_base_test.py
@@ -1,6 +1,19 @@
+import resource
+
 from locust import HttpUser, between
 
 from tests.conf import WIZARD_API_TOKEN
+
+resource.setrlimit(resource.RLIMIT_NOFILE, (10240, 9223372036854775807))
+
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+retry_strategy = Retry(
+    total=5,
+    backoff_factor=0.5,
+    status_forcelist=[429, 502, 503, 504],
+)
 
 
 class AuthorizedBaseTest(HttpUser):
@@ -10,3 +23,5 @@ class AuthorizedBaseTest(HttpUser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.headers = {'Authorization': f'Bearer {WIZARD_API_TOKEN}'}
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self.client.mount("https://", adapter)
